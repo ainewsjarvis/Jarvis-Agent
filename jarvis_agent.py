69d7f29b91efbbd3d5e2bf9089566acf1fbde2f4
ainewsjarvis <plainsignalai@gmail.com>
2026-07-30 17:35:19 +0200

    Update model version from gemini-1.5 to gemini-2.5

--- jarvis_agent.py
+++ jarvis_agent.py
@@ -13,7 +13,7 @@ GEMINI_API_KEY = "AIzaSyCXf10TO_1BzsyROPR_ulpg_qYEqPTnmAU"
 TELEGRAM_TOKEN = "8963063213:AAGiX2gq3fgDwf7_bPXO7SoZVzmKqM9qS-o"
 
 client = genai.Client(api_key=GEMINI_API_KEY)
-MODELL = "gemini-1.5-flash"
+MODELL = "gemini-2.5-flash"
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
